3ab1a21a6b03e94b79eb2e08a0a0a2c338842f4c
clarabrandt <mariaclarabrandt@gmail.com>
2018-09-14 12:22:28 +0200

    Objects and arrays

---
+++ BB-FlashCardApp-Started-Kit/index.js
@@ -0,0 +1,25 @@
+let places = {
+    flashCard1: ['What is the capital of Kenya?', 'Nairobi'],
+    flashCard2: ['In which country can we find a city called Oaxaca?', 'Mexico'],
+    flashCard3: ['Which is the most populated city in the world?', 'Tokyo'],
+    flashCard4: ['In which country can we find the Eiffel Tour?', 'France'],
+    flashCard5: ['What is the highest mountain on Earth?', 'Mount Everest']
+}
+let dutchEnglish = {
+    flashCard1: ['Good morning', 'goede morgen'],
+    flashCard2: ['Good bye/ see you!', 'Tot ziens'],
+    flashCard3: ['the bike', 'de fiets'],
+    flashCard4: ['aan appointment', 'een afspraak'],
+    flashCard5: ['the book', 'het boek']
+}
+let food = {
+    flashCard1: ['Pic 1', 'Food 1'],
+    flashCard2: ['Pic 2', 'Food 2'],
+    flashCard3: ['Pic 3', 'Food 3'],
+    flashCard4: ['Pic 4', 'Food 4'],
+    flashCard5: ['Pic 5', 'Food 5']
+}
+
+const flashCard = function () {
+
+}
